fde105835838ddf353baa0575f216bf21a91001c
pnaigd <prianandrei.gallardo@lorma.edu>
2026-01-27 13:31:36 +0800

    acts

--- LabActivity5.java
+++ LabActivity5.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class LabActivity5 {
 
     public static Scanner sc = new Scanner(System.in);
-    public static String[] strGames = new String[10];
 
     public static void main(String[] args) {
         int counter = 0;
@@ -12,7 +11,7 @@ public class LabActivity5 {
         boolean register = true;
         do {
             System.out.print("Enter Game Name: ");
-            String strGameName = assigner(InputString());
+            String strGameName = InputString();
 
             System.out.print("Enter Game Genre: ");
             String strGameGenre = InputString();
@@ -31,7 +30,6 @@ public class LabActivity5 {
             }
         } while (register);
         System.out.println("Number of Games regstered: [" + counter + "/10]");
-        print();
     }
 
     public static String InputString() {
@@ -48,19 +46,5 @@ public class LabActivity5 {
     public static double InputDouble() {
         double temp = sc.nextDouble();
         return temp;
-    }
-
-    public static String assigner(String strGameName) {
-        String GameName = "";
-        for (int i = 0; i < strGames.length; i++) {
-            strGames[i] = sc.nextLine();
-        }
-        return GameName;
-    }
-
-    public static void print() {
-        for (int i = 0; i < strGames.length; i++) {
-            System.out.println(strGames[i]);
-        }
     }
 }
